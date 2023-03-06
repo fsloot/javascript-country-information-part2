1cc40a36848e49c20fa6e29d25608940699836a7
fsloot <franksloothaak@gmail.com>
2023-03-06 20:37:40 +0100

    Het werkt maar styling nog niet helemaal af

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -560,18 +560,46 @@ function hmrAccept(bundle, id) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _axios = require("axios");
 var _axiosDefault = parcelHelpers.interopDefault(_axios);
-console.log("Hallo daar!");
+const searchResult = document.getElementById("country");
 async function getCountry(name) {
+    searchResult.innerHTML = ``;
     try {
-        const countryUrl = "https://restcountries.com/v2/name/" + name;
-        const result = await (0, _axiosDefault.default).get(countryUrl);
-        const country = result.data[0].name;
+        const result = await (0, _axiosDefault.default).get(`https://restcountries.com/v2/name/${name}`);
+        const country = result.data[0];
+        searchResult.innerHTML = `
+        <fieldset class="text-border">        
+            <div class = "flag-and-countryname">
+                <img class = "flag" src="${country.flag}" alt="Country flag">
+                <h2>${country.name}</h2>
+            </div>
+                <p>${country.name} is situated in ${country.subregion}. It has a population of ${country.population} people</p>
+                <p>The capital is ${country.capital} ${getCurrencies(country)}</p>
+                <p>They speak ${country.languages[0].name}.</p>
+        </fieldset>
+    `;
         console.log(country);
+        console.log(result.data[0].translations.nl);
     } catch (e) {
+        searchResult.innerHTML = `
+        <p>U hebt een niet bestaand land ingevoerd, een typefout gemaakt,</p>
+        <p>of er was een time-out in de netwerkverbinding. Probeer het nog eens!</p>
+    `;
         console.error(e);
     }
 }
-getCountry("Poland");
+function getCurrencies(specificCountry) {
+    let output = " and you can pay with ";
+    if (specificCountry.currencies.length === 2) return output + `${specificCountry.currencies[0].name}'s and ${specificCountry.currencies[1].name}'s`;
+    else return output + `${specificCountry.currencies[0].name}'s`;
+}
+function inputCountry(e) {
+    e.preventDefault();
+    const searchCountry = document.getElementById("search-country");
+    getCountry(searchCountry.value);
+    searchCountry.value = "";
+}
+const searchForm = document.getElementById("search-form");
+searchForm.addEventListener("submit", inputCountry);
 
 },{"axios":"jo6P5","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"jo6P5":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
